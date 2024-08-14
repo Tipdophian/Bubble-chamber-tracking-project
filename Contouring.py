655aa4b4d1f9d025c3804ebca83d5ba58f512f96
chanlokyee <157147758+chanlokyee@users.noreply.github.com>
2024-08-14 04:55:32 +0100

    Add files via upload

---
+++ Contouring.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 15:29:46 2024
+
+@author: Tiffany
+"""
+
+import cv2
+import numpy as np
+from Opening_images import image
+
+def contour(img): 
+    """
+    Detects and draws contours on an image.
+
+    Args:
+        img (numpy.ndarray): Input image.
+
+    Returns:
+        tuple: A tuple containing the image with drawn contours and a list of detected contours.
+
+    This function converts the input image to a binary image if necessary,
+    finds contours using OpenCV's `cv2.findContours`, and draws the contours
+    on the image. It returns both the image with contours and the detected contours.
+    """
+    if img.dtype == bool:
+        img = img.astype(np.uint8) * 255
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    if len(img.shape) == 2:  
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) 
+    img = cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
+    print('no. contours', len(contours))
+    return img, contours
+
+if __name__ == "__main__":
+    img = image(r"C:\Users\Tiffany\Downloads\COMPOS\Bubble Chamber Digital\bubble films (high res)\img_001[1].tif")
+    cv2.imshow('Original', img)
+    
+    img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    img, contours = contour(img)
+    cv2.imshow('Contoured', img)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
